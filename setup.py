7a2d9770df6ef847f8eb99e72b371617fb2dc897
sharath <sharathramku@umass.edu>
2018-11-19 11:44:34 +0100

    added repo structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "vgraph",
+    version = "0.0.1",
+    author = "Sharath Ramkumar",
+    author_email = "sharathramku@umass.edu",
+    description = ("A simple graph visualization library."),
+    license = "MNT",
+    keywords = "graph visualization vgraph",
+    url = "https://github.com/sharath/vgraph",
+    packages=find_packages()
+)
---
+++ vgraph/__init__.py
@@ -0,0 +1,3 @@
+__all__ = [
+    'graph', 'viz', 'algorithm', 'path'
+]
